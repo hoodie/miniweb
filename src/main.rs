16dc3b97c09b488f2478468912d53951b62182bc
Hendrik Sollich <hendrik@hoodie.de>
2016-11-02 21:08:58 +0100

    played around with form data

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,26 @@
 extern crate iron;
 extern crate router;
 extern crate mount;
+extern crate params;
 extern crate staticfile;
-extern crate websocket;
 
 #[macro_use]
 extern crate json;
 
 use std::path::Path;
 use std::io::{Error, ErrorKind};
-use std::time;
-use std::thread;
 use std::collections::HashMap;
-use std::io::Read;
 
 use iron::prelude::*;
 use iron::status;
 use iron::headers::ContentType;
 use iron::AfterMiddleware;
-use router::{Router, NoRoute, url_for};
+use router::{Router, NoRoute};
 use mount::Mount;
+use params::Params;
 use staticfile::Static;
 
-use websocket::{Server, Message};
-
-const HOST: &'static str = "localhost:3000";
-const WS_HOST: &'static str = "localhost:3001";
+const HOST: &'static str = "localhost:4000";
 
 struct CatchErrs;
 
@@ -35,14 +30,12 @@ impl AfterMiddleware for CatchErrs {
 
         if let Some(_) = err.error.downcast::<NoRoute>() {
             println!("   Some");
-            let mut resp = Response::with((status::InternalServerError,
-                                           format!("<h1>404</h1><pre>{:?}\n{}</pre>", req, err)));
+            let mut resp = Response::with((status::InternalServerError, format!("<h1>404</h1><pre>{:?}\n{}</pre>", req, err)));
             resp.headers.set(ContentType("text/html".parse().unwrap()));
             Ok(resp)
         } else {
-            println!("   Error: {:?}", err);
-            let mut resp = Response::with((status::InternalServerError,
-                                           format!("<h1>500</h1><pre>{:?}\n{}</pre>", req, err)));
+            println!("   Error: {:#?}", err);
+            let mut resp = Response::with((status::InternalServerError, format!("<h1>500</h1><pre>{:?}\n{}</pre>", req, err)));
             resp.headers.set(ContentType("text/html".parse().unwrap()));
             Ok(resp)
             // Err(err)
@@ -65,36 +58,49 @@ fn serve_id(req: &mut Request) -> IronResult<Response> {
     }
 }
 
-fn show_routes(req: &mut Request) -> IronResult<Response> {
-    Ok(
-        Response::with(
-            (status::Ok,
-             format!("{:?}", stringify!(foo)
-                    )
-            )
-            )
-      )
+fn post_print(req: &mut Request) -> IronResult<Response> {
+    let map = req.get_ref::<Params>().unwrap();
+    println!("map:\n{:?}", map);
+    Ok(Response::with((status::Ok, format!("you posted {:?}", map))))
+}
+
+fn post_shout(req: &mut Request) -> IronResult<Response> {
+    let map = req.get_ref::<Params>().unwrap();
+    let mut new_map = HashMap::new();
+
+    for (k, v) in map.iter() {
+        if let &params::Value::String(ref v) = v {
+            new_map.insert(k, v.to_uppercase());
+        }
+    }
+
+    Ok(Response::with((status::Ok, format!("you posted {:?}", new_map))))
+}
+
+fn show_routes(_req: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, format!("{:?}", stringify!(not implemented sorry)))))
 }
 
 fn setup_static_server() {
     println!("http listening on: http://{}", HOST);
 
-    let content_path = Path::new("content/");
-    let assets_path = Path::new("assets/");
+    let content_path = Path::new("./htdocs/");
 
     let mut router = Router::new();
     router.get("/", Static::new(content_path), "index");
+    router.get("/", Static::new(content_path), "index");
+    router.get("/*", Static::new(content_path), "content");
     router.get("/routes", show_routes, "routes");
     router.get("/routes/", show_routes, "routes/");
-    //router.get("/*", Static::new(content_path), "content");
-    router.get("/images/*", Static::new(assets_path), "images");
     router.get("/items/:id", serve_id, "items");
+    router.post("/print_name", post_print, "post_print");
+    router.post("/shout_name", post_shout, "post_shout");
 
     let mut mount = Mount::new();
     mount.mount("/", router);
+    mount.mount("/images/", Static::new("./htdocs/assets/images/"));
     mount.mount("/docs/", Static::new("target/doc"));
-    mount.mount("/favicon.ico",
-                Static::new(Path::new("assets/images/favicon.png")));
+
 
     let mut chain = Chain::new(mount);
     chain.link_after(CatchErrs);
@@ -102,39 +108,7 @@ fn setup_static_server() {
     Iron::new(chain).http(HOST).unwrap();
 }
 
-fn setup_websocket_server() {
-    println!("websocket listening on: http://{}", WS_HOST);
-
-    let ws_server = Server::bind(WS_HOST).unwrap();
-    for connection in ws_server {
-        thread::spawn(move || {
-            let request = connection.unwrap().read_request().unwrap(); // Get the request
-            let response = request.accept(); // Form a response
-
-            let mut client = response.send().unwrap(); // Send the response
-
-
-            loop {
-                let now = time::SystemTime::now();
-                let message = Message::text(json::stringify(object!{
-                    "timestamp" => format!("{:?}",now),
-                    "color"  => object!{
-                        "R" => 0,
-                        "G" => 0,
-                        "B" => 255
-                    },
-                    "answer" => 42
-
-
-                        }));
-                let _ = client.send_message(&message);
-                thread::sleep(time::Duration::from_millis(5000));
-            }
-        });
-    }
-}
 
 fn main() {
-    //thread::spawn(|| setup_websocket_server());
     setup_static_server();
 }
